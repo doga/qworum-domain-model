6cc86157ff23bfc51f927987ef33f2b7d4ae56e5
Doğa Armangil <doga.armangil@alumni.epfl.ch>
2025-04-24 23:01:45 +0200

    ids can now be written to and read from rdf datasets.

--- test/id_test.ts
+++ test/id_test.ts
@@ -1,7 +1,27 @@
 // deno test --allow-import ./test/id_test.mts
 
 import {assertInstanceOf, assertEquals, assertNotEquals, assertThrows, assertFalse, assert } from "jsr:@std/assert@1";
-import { Id, OrgId, GroupId, UserId, PasswordId, userid, orgid } from '../mod.mjs';
+import {IRI, iri, Id, PersonaId, CollabId, OrgId, GroupId, UserId, PasswordId, userid, orgid } from '../mod.mjs';
+
+
+Deno.test('ids can be written as rdf and then read back', () => {
+  [CollabId, OrgId, GroupId, UserId, PersonaId].forEach(
+    idClass => {
+      const
+      id      = idClass.uuid(),
+      dataset = id.toDataset(),
+      ids     = idClass.readFrom(dataset);
+    
+      // console.debug(`id`,id);
+      // console.debug(`dataset`,dataset);
+      // console.debug(`ids`,ids);
+    
+      assertEquals(`${id}`, `${ids[0]}`);
+    }
+  );
+
+});
+
 
 Deno.test('generate password id', () => {
   const id = new PasswordId('urn:qworum:password:1234');
