9616f652863cfd8de73394f12e7388d1b5568638
Doğa Armangil <doga.armangil@alumni.epfl.ch>
2025-07-22 17:58:22 +0200

    update tests

--- test/id_test.js
+++ test/id_test.js
@@ -1,6 +1,6 @@
 // deno test --allow-import ./test/id_test.js
 
-import {assertInstanceOf, assertEquals, assertNotEquals, assertThrows, assertFalse, assert } from "jsr:@std/assert@1";
+import {assertInstanceOf, assertNotInstanceOf, assertEquals, assertNotEquals, assertThrows, assertFalse, assert } from "jsr:@std/assert@1";
 import {
   IRI, iri, Id, OrgId, GroupId, UserId, MembershipId, PartnershipId, partnership_id, PartnershipMembershipId, partnership_membership_id, PasswordId, user_id, org_id,
   UserIdSet, GroupIdSet,
@@ -32,23 +32,94 @@ Deno.test('ids can be written as rdf and then read back', () => {
 
 
 Deno.test('user id set', () => {
+  // add, has
   const
   id1 = UserId.uuid(),
   id2 = UserId.uuid(),
   ids = new UserIdSet().add(id1).add(id2);
 
   // console.debug(`[test] id1: ${id1}`, id1);
-  console.debug(`[test] ids:`, ids.members);
-  console.debug(`[test] ids.size:`, ids.size);
-
+  // console.debug(`[test] ids:`, ids.members);
+  // console.debug(`[test] ids.size:`, ids.size);
 
   assert(ids.has(id1));
   assert(ids.has(id2));
   assertEquals(ids.size, 2);
+
+  // remove
   ids.remove(id2);
   assertEquals(ids.size, 1);
   assert(ids.has(id1));
   assertFalse(ids.has(id2));
+
+  // clone, isSameAs, isSubsetOf, isSupersetOf
+  const
+  idsClone = ids.clone();
+  assertEquals(idsClone.size, ids.size);
+  assert(idsClone.has(id1));
+  assertFalse(idsClone.has(id2));
+  assert(idsClone.isSameAs(ids));
+  assert(idsClone.isSupersetOf(ids));
+  assert(idsClone.isSubsetOf(ids));
+
+  // clear
+  const
+  idsCleared = ids.clone();
+  idsCleared.clear();
+  assert(idsCleared.isEmpty);
+
+  // set operations
+  const
+  id3  = UserId.uuid(),
+  id4  = UserId.uuid(),
+  ids2 = new UserIdSet().add(id3).add(id4),
+
+  ids_ids2_intersection        = ids.intersection(ids2),
+  ids_ids2_union               = ids.union(ids2),
+  ids_ids2_symmetricDifference = ids.symmetricDifference(ids2);
+
+  assert(
+    ids_ids2_intersection.members.every(
+      id => (
+        ids.has(id) && ids2.has(id)
+      )
+    )
+  );
+
+  assert(
+    ids_ids2_union.members.every(
+      id => (
+        ids.has(id) || ids2.has(id)
+      )
+    )
+  );
+
+  assertNotInstanceOf(
+    ids_ids2_symmetricDifference.members.find(
+      id => (
+        ids.has(id) && ids2.has(id)
+      )
+    ),
+    UserId
+  );
+
+  // no duplicates
+  const
+  ids3 = new UserIdSet().add([id1, id2, id3, id3, id1]);
+
+  let idDuplicate = false;
+  
+  for (let i = 0; i < ids3.members.length; i++) {
+    const idi = ids3.members[i];
+    for (let j = 0; j < ids3.members.length; j++) {
+      if(i === j)continue;
+      const idj = ids3.members[j];
+      assertFalse(idi.equals(idj));
+    }
+  }
+
+
+
 });
 
 
